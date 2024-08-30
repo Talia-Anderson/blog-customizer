b69c38bb3fd6900a7800d304ce35ec1e64630fdf
Natalia Shebekina <lady.malfoy1995@yandex.ru>
2024-08-30 20:17:55 +0300

    all done

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,18 +1,28 @@
 import arrow from 'src/images/arrow.svg';
+import React from 'react';
 
 import styles from './ArrowButton.module.scss';
 
-/** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+interface ArrowButtonProps {
+	onClick?: OnClick; // Сделаем onClick необязательным
+}
+
+export const ArrowButton = ({ onClick }: ArrowButtonProps) => {
 	return (
-		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
+			className={styles.container}
+			onClick={onClick} // Если onClick передан, он будет вызван
+			onKeyDown={(e) => {
+				if ((e.key === 'Enter' || e.key === ' ') && onClick) {
+					e.preventDefault();
+					onClick();
+				}
+			}}>
 			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
 		</div>
 	);
